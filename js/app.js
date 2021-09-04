c8b73bb276cb3eb767cc342e20fc1c1a03e8eb21
mohon63 <mohonhossen63@gmail.com>
2021-09-05 00:11:36 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const API_KEY = `4eea3f400af0c2391671f51f2bbccf39`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('conditon', temperature.weather[0].main);
+
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+
+};
